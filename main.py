344dfe6bd7d5afba2441ced40555d816fef52697
Roxas-9E-ay2526 <roxas.cv@obmontessori.edu.ph>
2026-08-13 11:10:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,4 @@ student_dict = {'color': 'blue', 'car_brand': 'Tesla','shoe_size': '11','best_fr
 fruit_favorite = {"Watermelon", "Mango", "Papayas", "Strawberry", "Grape"}
 days_of_the_week = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday",)
 
-
-display(f"{name}", target="output")
-display(f"{age}", target="output")
-display(f"{height167}", target="output")
-display(f"{targeted_countries}", target="output")
-display(f"{student_type}", target="output")
-display(f"{student_dict}", target="output")
-display(f"{fruit_favorite}", target="output")
-display(f"{days_of_the_week}", target="output")
-
 document.getElementById('result').innerHTML = f'Hello My name is <i>{name}</i>, I am {age} years old, My height is {height167}cm, the countries I plan on vissiting in the future are {targeted_countries},my student past is {student_type}, and my student dictionary is {student_dict}.,My favorite fruits are {fruit_favorite}, and here are the seven days of the week {days_of_the_week}' 
